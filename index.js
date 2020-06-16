082902309edd385e4cd5766286cdfe72f06e30fa
pharia-le <pharia.le@gmail.com>
2020-06-16 16:16:23 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Your code here
+const mapToNegativize = (arr) => {
+    return arr.map(x => x*(-1))
+}
+
+const mapToNoChange = (arr) => {
+    return arr
+}
+
+const mapToDouble = (arr) => {
+    return arr.map(x => x*2)
+}
+
+const mapToSquare = (arr) => {
+    return arr.map(x => x*x)
+}
+
+const reduceToTotal = (arr,memo=0) => {
+    return arr.reduce((x,y) => x+y,memo)
+}
+
+const reduceToAllTrue = (arr) => {
+    for (const ele of arr) {
+        if (!!ele === false) {
+            return false
+        }
+    }
+    return true
+}
+// arr.reduce(callback( accumulator, currentValue[, index[, array]] ))
+const reduceToAnyTrue = (arr) => {
+    for (const ele of arr) {
+        if (!!ele) {
+            return true
+        }
+    }
+    return false
+}
